7c13522969affbfac6278dad5e764ba00a0f8dfc
Gabor Szabo <gabor@szabgab.com>
2026-05-14 14:49:36 +0300

    split to functions

--- examples/python-demo/motogp.py
+++ examples/python-demo/motogp.py
@@ -1,40 +1,67 @@
+import sys
 from falkordb import FalkorDB
 
 
-def main():
-    # Connect to FalkorDB
-    db = FalkorDB(host="localhost", port=6379)
+def load(graph):
 
-    # Create the 'MotoGP' graph
-    g = db.select_graph("MotoGP")
-
-    # Clear out this graph in case you've run this script before.
-    # Check if the graph exists before trying to delete it.
-    try:
-        g.delete()
-    except Exception:
-        # Graph doesn't exist yet, which is fine
-        pass
-
-    g.query("""CREATE
+    graph.query("""CREATE
                (:Rider {name:'Valentino Rossi'})-[:rides]->(:Team {name:'Yamaha'}),
                (:Rider {name:'Dani Pedrosa'})-[:rides]->(:Team {name:'Honda'}),
                (:Rider {name:'Andrea Dovizioso'})-[:rides]->(:Team {name:'Ducati'})""")
 
+
+def which_rider_represents_yamaha(graph):
     # Query which riders represent Yamaha?
-    res = g.query("""MATCH (r:Rider)-[:rides]->(t:Team)
+    res = graph.query("""MATCH (r:Rider)-[:rides]->(t:Team)
                      WHERE t.name = 'Yamaha'
                      RETURN r.name""")
 
     for row in res.result_set:
         print(row[0])  # Prints: "Valentino Rossi"
 
+
+def how_many_riders_represent_ducati(graph):
     # Query how many riders represent team Ducati ?
-    res = g.query(
+    res = graph.query(
         """MATCH (r:Rider)-[:rides]->(t:Team {name:'Ducati'}) RETURN count(r)"""
     )
 
     print(res.result_set[0][0])  # Prints: 1
+
+
+def delete(graph):
+    try:
+        graph.delete()
+    except Exception:
+        # Graph doesn't exist yet, which is fine
+        pass
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        usage()
+    cmd = sys.argv[1]
+    if cmd not in DISPATCH:
+        usage()
+
+    db = FalkorDB(host="localhost", port=6379)
+    graph = db.select_graph("MotoGP")
+
+    DISPATCH[cmd](graph)
+
+
+def usage():
+    cmds = ", ".join(sorted(DISPATCH.keys()))
+    print(f"Usage: {sys.argv[0]} {cmds}")
+    exit(1)
+
+
+DISPATCH = {
+    "load": load,
+    "delete": delete,
+    "yamaha": which_rider_represents_yamaha,
+    "ducati": how_many_riders_represent_ducati,
+}
 
 
 if __name__ == "__main__":
