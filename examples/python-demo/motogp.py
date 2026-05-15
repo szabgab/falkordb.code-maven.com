5ceb1ca9e6844b5a0df847e6071c85ef96a69eea
Gabor Szabo <gabor@szabgab.com>
2026-05-15 22:32:15 +0300

    update page an example

--- examples/python-demo/motogp.py
+++ examples/python-demo/motogp.py
@@ -9,12 +9,41 @@ def load(graph):
                (:Rider {name:'Dani Pedrosa'})-[:rides]->(:Team {name:'Honda'}),
                (:Rider {name:'Andrea Dovizioso'})-[:rides]->(:Team {name:'Ducati'})""")
 
+def all_riders(graph):
+    res = graph.query("""MATCH (r:Rider)
+                     RETURN r""")
+
+    # print(type(res.result_set))
+    # `result_set` is a `list` of tuples.
+    # In this case they are 1-element tuples. (Hence the comma)
+    for rider, in res.result_set:
+        print(rider.properties['name'])
+
+def all_pairs(graph):
+    res = graph.query("""MATCH (r:Rider)-[:rides]->(t:Team)
+                     RETURN r, t""")
+
+    for rider, team in res.result_set:
+        print(f"{rider.properties['name']:18} - {team.properties['name']}")
+        #alias', 'id', 'labels', 'properties', 'to_string'
+
+
+def all_names(graph):
+    res = graph.query("""MATCH (r:Rider)-[:rides]->(t:Team)
+                     RETURN r.name, t.name""")
+
+    for rider, team in res.result_set:
+        print(f"{rider:18} - {team}")
+
 
 def which_rider_represents_yamaha(graph):
     # Query which riders represent Yamaha?
+    company_name = 'Yamaha'
+    #company_name = 'Honda'
     res = graph.query("""MATCH (r:Rider)-[:rides]->(t:Team)
-                     WHERE t.name = 'Yamaha'
-                     RETURN r.name""")
+                     WHERE t.name = $value
+                     RETURN r.name""",
+                      { "value": company_name })
 
     for row in res.result_set:
         print(row[0])  # Prints: "Valentino Rossi"
@@ -22,8 +51,11 @@ def which_rider_represents_yamaha(graph):
 
 def how_many_riders_represent_ducati(graph):
     # Query how many riders represent team Ducati ?
+    company_name = 'Ducati'
     res = graph.query(
-        """MATCH (r:Rider)-[:rides]->(t:Team {name:'Ducati'}) RETURN count(r)"""
+        #"""MATCH (r:Rider)-[:rides]->(t:Team {name:'Ducati'}) RETURN count(r)"""
+        #"""MATCH (r:Rider)-[:rides]->(t:Team) WHERE t.name = 'Ducati' RETURN count(r)"""
+        """MATCH (r:Rider)-[:rides]->(t:Team) WHERE t.name = $name RETURN count(r)""", { "name" : company_name }
     )
 
     print(res.result_set[0][0])  # Prints: 1
@@ -61,6 +93,9 @@ DISPATCH = {
     "delete": delete,
     "yamaha": which_rider_represents_yamaha,
     "ducati": how_many_riders_represent_ducati,
+    "all_names": all_names,
+    "all_riders": all_riders,
+    "all_pairs": all_pairs,
 }
 
 
--- src/python-motogp.md
+++ src/python-motogp.md
@@ -1,9 +1,25 @@
 # MotoGP
 
-{% embed include file="examples/python-demo/motogp.py" %}
+This example is based on the one on the front page of the [FalkorDB documentation](https://docs.falkordb.com/)
+
+Run the following to show the "usage":
 
 ```shell
+$ cd examples/python-demo
 $ uv run motogp.py
 ```
+
+The `main` function sets up the connection to the database and selects the graph called **MotoGP**.
+
+* `$ uv run motogp.py delete` - Delete the existing graph (in case we had it already in the database):
+* `$ uv run motogp.py load`   - `CREATE` a few nodes and relationships.
+* `$ uv run motogp.py all_riders` - List the names of the riders.
+* `$ uv run motogp.py all_pairs` - Get back all the Rider-rides-Team relations and print out the names.
+* `$ uv run motogp.py all_names` - The same, but now we only return the names.
+* `$ uv run motogp.py yamaha` - Which riders represent Yamaha?
+* `$ uv run motogp.py ducati` - How many riders represent team Ducati? - An aggregator.
+
+
+{% embed include file="examples/python-demo/motogp.py" %}
 
 
