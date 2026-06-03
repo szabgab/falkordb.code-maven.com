de326287ad0dbffc105808d6cc2486c6f0ee81db
Gabor Szabo <gabor@szabgab.com>
2026-06-03 17:25:30 +0300

    path

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-const prefixes = ["src/examples/", "exemples/"];
+const prefixes = ["src/examples/", "examples/"];
 const extensions = [".py", ".toml", ".cfg"];
 const base_url = "https://github.com/szabgab/python.code-maven.com/tree/main/books/python-testing/"
 
