0f57e298d4269571682bb82b6fdb0a140314beec
Gabor Szabo <gabor@szabgab.com>
2026-06-04 16:33:05 +0300

    show more extensions

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
 const prefixes = ["src/examples/", "examples/"];
-const extensions = [".py", ".toml", ".cfg", ".rs", ".pl", ".pm"];
+const extensions = [".py", ".toml", ".cfg", ".rs", ".pl", ".pm", ".yaml"];
 const base_url = "https://github.com/szabgab/falkordb.code-maven.com/tree/main/"
 
